e462a4419fdf1c8614511ee72457c832c4bb9b38
Alexandre Aubert <alexandre.aubert@heig.vd.ch>
2023-04-03 11:54:56 +0200

    first add .

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const baseKelvin = ref(0);
+const celsiusValue = computed(() => baseKelvin.value - 273.15);
+const fahrenheitValue = computed(() => (baseKelvin.value * 9) / 5 - 459.67);
+
+
+function updateBaseKelvin(event) {
+  baseKelvin.value = event.target.value;
+}
+</script>
+
+<template>
+  <label>Kelvin</label>
+  <input type="number" :value="baseKelvin" @input="updateBaseKelvin($event)"/>
+  <label>Celsius</label>
+  <input type="number" :value="celsiusValue"/>
+  <label>Fahrenheit</label>
+  <input type="number" :value="fahrenheitValue"/>
+</template>
+
+<style scoped>
+  * {
+    display: block;
+  }
+  label {
+    margin-top: 1rem;
+  }
+</style>
